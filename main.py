ad9994d3fadc9f85ca4d7e060163171fb0bac317
Alexia <alexiakabore2@gmail.com>
2026-04-19 17:56:30 -0400

    ajout de salle

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from Models.salle import Salle
 
 dao = DataSalle()
 
-s1 = Salle("S1", "Salle Info", "Laboratoire", 30)
-
+s1 = Salle("S10", "Salle Info", "Laboratoire", 30)
 dao.insert_salle(s1)
+
+s2 = Salle("S12", "Salle Reunion", "Bureau", 50)
+dao.insert_salle(s2)
+
+s3 = Salle("S11", "Grande Salle", "Classe", 60)
+dao.insert_salle(s3)

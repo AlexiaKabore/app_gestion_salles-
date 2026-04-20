3bc0df072a45b0d3b359ad3236141523589099dc
Alexia <alexiakabore2@gmail.com>
2026-04-19 22:57:07 -0400

    Ajout fonction ajouter_salle avec liaison bouton

--- views/view_salle.py
+++ views/view_salle.py
@@ -8,7 +8,6 @@ class ViewSalle(ctk.CTk):
 
         self.title("Gestion des salles")
         self.geometry("500x500")
-
         self.service_salle = ServiceSalle()
 
         self.cadreInfo = ctk.CTkFrame(self)
@@ -33,3 +32,30 @@ class ViewSalle(ctk.CTk):
         self.label_cap.grid(row=3, column=0)
         self.entry_cap = ctk.CTkEntry(self.cadreInfo)
         self.entry_cap.grid(row=3, column=1)
+
+        self.cadreActions = ctk.CTkFrame(self)
+        self.cadreActions.pack(pady=10)
+
+        self.btn_ajouter = ctk.CTkButton(self.cadreActions, text="Ajouter", command=self.ajouter_salle)
+        self.btn_ajouter.grid(row=0, column=0, padx=5)
+
+        self.btn_modifier = ctk.CTkButton(self.cadreActions, text="Modifier")
+        self.btn_modifier.grid(row=0, column=1, padx=5)
+
+        self.btn_supprimer = ctk.CTkButton(self.cadreActions, text="Supprimer")
+        self.btn_supprimer.grid(row=0, column=2, padx=5)
+
+        self.btn_rechercher = ctk.CTkButton(self.cadreActions, text="Rechercher")
+        self.btn_rechercher.grid(row=0, column=3, padx=5)
+
+    def ajouter_salle(self):
+        code = self.entry_code.get()
+        libelle = self.entry_libelle.get()
+        type_s = self.entry_type.get()
+        capacite = int(self.entry_cap.get())
+
+        from Models.salle import Salle
+        salle = Salle(code, libelle, type_s, capacite)
+
+        resultat, message = self.service_salle.ajouter_salle(salle)
+        print(message)

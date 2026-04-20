edbbf46a3b3720e8e92a715c6f9ecd02842b3fe1
Alexia <alexiakabore2@gmail.com>
2026-04-19 21:09:52 -0400

    Ajout du cadre informations salle (labels et champs)

--- views/view_salle.py
+++ views/view_salle.py
@@ -10,3 +10,26 @@ class ViewSalle(ctk.CTk):
         self.geometry("500x500")
 
         self.service_salle = ServiceSalle()
+
+        self.cadreInfo = ctk.CTkFrame(self)
+        self.cadreInfo.pack(pady=10)
+
+        self.label_code = ctk.CTkLabel(self.cadreInfo, text="Code")
+        self.label_code.grid(row=0, column=0)
+        self.entry_code = ctk.CTkEntry(self.cadreInfo)
+        self.entry_code.grid(row=0, column=1)
+
+        self.label_libelle = ctk.CTkLabel(self.cadreInfo, text="Libellé")
+        self.label_libelle.grid(row=1, column=0)
+        self.entry_libelle = ctk.CTkEntry(self.cadreInfo)
+        self.entry_libelle.grid(row=1, column=1)
+
+        self.label_type = ctk.CTkLabel(self.cadreInfo, text="Type")
+        self.label_type.grid(row=2, column=0)
+        self.entry_type = ctk.CTkEntry(self.cadreInfo)
+        self.entry_type.grid(row=2, column=1)
+
+        self.label_cap = ctk.CTkLabel(self.cadreInfo, text="Capacité")
+        self.label_cap.grid(row=3, column=0)
+        self.entry_cap = ctk.CTkEntry(self.cadreInfo)
+        self.entry_cap.grid(row=3, column=1)
